a0d48f8ee8d54846e39708d107542eeb4f022838
HJay <exist91@gmail.com>
2015-01-23 23:43:01 +0900

    #1 주사위 구현

--- src/main/java/com/maple/diceGame/Dice.java
+++ src/main/java/com/maple/diceGame/Dice.java
@@ -2,4 +2,8 @@ package com.maple.diceGame;
 
 public class Dice {
 
+	public int draw(String mode) {
+		
+		return (int) (Math.random() * 1000) % 6 + 1;// 1~6
+	}
 }
--- src/main/java/com/maple/diceGame/FraudDice.java
+++ src/main/java/com/maple/diceGame/FraudDice.java
@@ -1,5 +1,34 @@
 package com.maple.diceGame;
 
-public class FraudDice {
+public class FraudDice extends Dice {
+
+	private String mode = "일반";
+
+	public String getMode() {
+		return mode;
+	}
+
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+
+	@Override
+	public int draw(String mode) {
+		switch (mode) {
+		case "일반": {
+			return (int) (Math.random() * 1000) % 6 + 1;// 1~6
+		}
+		case "강함": {
+			return (int) (Math.random() * 1000) % 4 + 3;// 3~6
+		}
+		case "약함": {
+			return (int) (Math.random() * 1000) % 4 + 1;// 1~4
+		}
+		default: {
+			System.out.println("모드 설정이 잘못됨.");
+			return -1;
+		}
+		}
+	}
 
 }
